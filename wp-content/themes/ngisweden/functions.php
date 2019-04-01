eb802a68ebdefed60d427b44d9fd5a5a9c3d276f
Phil Ewels <phil.ewels@scilifelab.se>
2019-04-01 16:35:14 +0200

    Add titles to NGI widgets

--- wp-content/themes/ngisweden/functions.php
+++ wp-content/themes/ngisweden/functions.php
@@ -163,7 +163,7 @@ remove_action( 'admin_color_scheme_picker', 'admin_color_scheme_picker' );
 
 
 // Add theme widgets for footer
-class FooterLogos extends WP_Widget {
+class FooterUniLogos extends WP_Widget {
     public function __construct() {
         $widget_opts = array(
             'classname' => 'ngisweden-footer-logos',
@@ -172,6 +172,10 @@ class FooterLogos extends WP_Widget {
         parent::__construct( 'ngisweden_footer_logos', 'NGI Footer Logos', $widget_opts);
     }
     public function widget( $args, $instance ) {
+        echo $args['before_widget'];
+        if ( ! empty( $instance['title'] ) ) {
+            echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
+        }
         echo '<div class="ngisweden-footer-logos">
             <a href="https://www.scilifelab.se" target="_blank"><img src="'.get_stylesheet_directory_uri().'/img/SciLifeLab-logo.svg" class="footer-scilifelab-logo" alt="SciLifeLab logo"></a>
             <br>
@@ -180,6 +184,67 @@ class FooterLogos extends WP_Widget {
             <a href="https://www.su.se" target="_blank"><img src="'.get_stylesheet_directory_uri().'/img/SU-logo.svg" class="footer-uni-logo" alt="SU logo"></a>
             <a href="https://www.uu.se" target="_blank"><img src="'.get_stylesheet_directory_uri().'/img/UU-logo.svg" class="footer-uni-logo" alt="UU logo"></a>
         </div>';
+        echo $args['after_widget'];
+    }
+    // Admin interface
+    public function form( $instance ) {
+        $title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'text_domain' );
+        ?>
+        <p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'text_domain' ); ?></label>
+            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+        </p>
+        <?php
+    }
+    // Save sanitation
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
+        return $instance;
     }
 }
-add_action( 'widgets_init', function(){ register_widget( 'FooterLogos' ); });
+add_action( 'widgets_init', function(){ register_widget('FooterUniLogos'); });
+
+class FooterSocialButtons extends WP_Widget {
+    public function __construct() {
+        $widget_opts = array(
+            'classname' => 'ngisweden-footer-social',
+            'description' => 'Twitter, YouTube and GitHub buttons',
+        );
+        parent::__construct( 'ngisweden_footer_social', 'NGI Footer Social Buttons', $widget_opts);
+    }
+    // Front end
+    public function widget( $args, $instance ) {
+        echo $args['before_widget'];
+        if ( ! empty( $instance['title'] ) ) {
+            echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
+        }
+        echo '<div class="ngisweden-footer-social">
+            <a href="https://twitter.com/ngisweden?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @ngisweden</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            <br>
+            <script src="https://apis.google.com/js/platform.js"></script>
+            <div class="g-ytsubscribe" data-channelid="UC7vNQ8xMt2zViIdtngTCTsw" data-layout="default" data-count="default"></div>
+            <br>
+            <a class="github-button" href="https://github.com/NationalGenomicsInfrastructure" aria-label="Follow @NationalGenomicsInfrastructure on GitHub">Follow @NationalGenomicsInfrastructure</a>
+            <script async defer src="https://buttons.github.io/buttons.js"></script>
+        </div>';
+        echo $args['after_widget'];
+    }
+    // Admin interface
+    public function form( $instance ) {
+        $title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'text_domain' );
+        ?>
+        <p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'text_domain' ); ?></label>
+            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+        </p>
+        <?php
+    }
+    // Save sanitation
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
+        return $instance;
+    }
+}
+add_action( 'widgets_init', function(){ register_widget('FooterSocialButtons'); });
