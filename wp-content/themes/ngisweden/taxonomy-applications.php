609bade4f5cdb8a40c82d316944984418683e671
Phil Ewels <phil.ewels@scilifelab.se>
2019-04-01 17:02:56 +0200

    Always show methods if assigned to an application

--- wp-content/themes/ngisweden/taxonomy-applications.php
+++ wp-content/themes/ngisweden/taxonomy-applications.php
@@ -24,19 +24,19 @@
 
   echo '<h1>'.$page_title.'</h1>';
   echo $page_intro;
+  $postcounter = -1;
 
   // Get term children, if there are any
   $term_children = get_term_children($taxonomy->term_id, 'applications');
   if(count($term_children) > 0){
+    $postcounter++;
     // Show child terms, not methods
     echo '<div class="ngisweden-application-methods card-deck">';
-    $postcounter = 0;
     foreach ($term_children as $child) {
       // Get the sub-term details
       $subterm = get_term_by('id', $child, 'applications' );
       $subterm_app_description = trim(strip_tags(term_description($child, 'applications')));
       // Start printing this term in a card
-      $postcounter++;
       if($postcounter % 3 == 0){
         echo '<div class="ngisweden-application-methods card-deck">';
       }
@@ -57,62 +57,56 @@
         echo '</div>';
       }
     }
-    if($postcounter % 3 != 2){
-      echo '</div>';
-    }
   }
 
-  else {
-    // Loop through the methods in this application and show snippets
-    if (have_posts()) {
-      $postcounter = null;
-      while (have_posts()) {
-        the_post();
-        $postcounter = $wp_query->current_post;
-        if($wp_query->current_post % 3 == 0){
-          echo '<div class="ngisweden-application-methods card-deck">';
-        }
-        ?>
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">
-              <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            </h5>
-            <?php
-            // Excerpt intro text
-            if(has_excerpt()) {
-              echo '<p class="card-text">'.strip_tags(get_the_excerpt()).'</p>';
-            }
-            // General keywords
-            $method_keywords = get_the_terms(null, 'method_keywords');
-            if ($method_keywords && !is_wp_error($method_keywords)){
-              foreach($method_keywords as $kw){
-                echo '<a href="'.get_term_link($kw->slug, 'method_keywords').'" rel="tag" class="badge badge-secondary method-keyword '.$kw->slug.'">'.$kw->name.'</a> ';
-              }
-            }
-            // Sequencing type
-            $method_seqtypes = get_the_terms(null, 'sequencing_type');
-            if ($method_seqtypes && !is_wp_error($method_seqtypes)){
-              foreach($method_seqtypes as $kw){
-                echo '<a href="'.get_term_link($kw->slug, 'sequencing_type').'" rel="tag" class="badge badge-info method-keyword '.$kw->slug.'">'.$kw->name.'</a> ';
-              }
-            }
-            ?>
-
-          </div>
-          <div class="card-footer">
-            <a href="<?php the_permalink(); ?>" class="btn btn-primary btn-sm">Read more</a>
-          </div>
-        </div>
-        <?php
-        if($wp_query->current_post % 3 == 2){
-          echo '</div>';
-        }
+  // Loop through the methods in this application and show snippets
+  if (have_posts()) {
+    while (have_posts()) {
+      the_post();
+      $postcounter++;
+      if($postcounter % 3 == 0){
+        echo '<div class="ngisweden-application-methods card-deck">';
       }
-      if($postcounter % 3 != 2){
+      ?>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+          </h5>
+          <?php
+          // Excerpt intro text
+          if(has_excerpt()) {
+            echo '<p class="card-text">'.strip_tags(get_the_excerpt()).'</p>';
+          }
+          // General keywords
+          $method_keywords = get_the_terms(null, 'method_keywords');
+          if ($method_keywords && !is_wp_error($method_keywords)){
+            foreach($method_keywords as $kw){
+              echo '<a href="'.get_term_link($kw->slug, 'method_keywords').'" rel="tag" class="badge badge-secondary method-keyword '.$kw->slug.'">'.$kw->name.'</a> ';
+            }
+          }
+          // Sequencing type
+          $method_seqtypes = get_the_terms(null, 'sequencing_type');
+          if ($method_seqtypes && !is_wp_error($method_seqtypes)){
+            foreach($method_seqtypes as $kw){
+              echo '<a href="'.get_term_link($kw->slug, 'sequencing_type').'" rel="tag" class="badge badge-info method-keyword '.$kw->slug.'">'.$kw->name.'</a> ';
+            }
+          }
+          ?>
+
+        </div>
+        <div class="card-footer">
+          <a href="<?php the_permalink(); ?>" class="btn btn-primary btn-sm">View method</a>
+        </div>
+      </div>
+      <?php
+      if($postcounter % 3 == 2){
         echo '</div>';
       }
     }
+  }
+  if($postcounter % 3 != 2){
+    echo '</div>';
   }
 
   // Echo the rest of the page contents from the linked page
