e95cca607c642d9f321483c2c5b27749268907d1
Phil Ewels <phil.ewels@scilifelab.se>
2019-04-02 20:09:01 +0200

    Organise theme code into files

---
+++ wp-content/themes/ngisweden/functions/admin_ui.php
@@ -0,0 +1,76 @@
+<?php
+
+// Allow styling in the gutenberg editor to match front-end
+function ngisweden_guten_block_editor_assets() {
+    wp_enqueue_style(
+        'ngisweden-editor-style',
+        get_stylesheet_directory_uri() . "/editor.css",
+        array(),
+        '1.0'
+    );
+}
+add_action('enqueue_block_editor_assets', 'ngisweden_guten_block_editor_assets');
+
+
+// Clean up the admin interface
+
+// We don't have comments on this site, so remove it
+function ngisweden_admin_menu_cleanup() { remove_menu_page('edit-comments.php'); }
+add_action( 'admin_menu', 'ngisweden_admin_menu_cleanup' );
+
+function my_admin_bar_render() {
+    global $wp_admin_bar;
+    $wp_admin_bar->remove_menu('wp-logo');
+    $wp_admin_bar->remove_menu('comments');
+    $wp_admin_bar->remove_node('new-media');
+    $wp_admin_bar->remove_node('new-cptbc');
+    $wp_admin_bar->remove_node('new-user');
+    // Rename posts to news
+    $new_post_node = $wp_admin_bar->get_node('new-post');
+    $new_post_node->title = 'News';
+    $wp_admin_bar->remove_node('new-post'); # Remove first so that it's at the bottom
+    $wp_admin_bar->add_node($new_post_node);
+}
+add_action( 'wp_before_admin_bar_render', 'my_admin_bar_render' );
+
+// Customise the order of pages in the admin list (move Media down)
+function ngisweden_admin_menu_media_down() {
+    return array(
+        'index.php',                  // Dashboard
+        'edit.php',                   // Posts
+        'edit.php?post_type=methods', // Methods
+        'edit.php?post_type=bioinformatics', // Methods
+        'edit.php?post_type=page',    // Pages
+        'edit.php?post_type=cptbc',   // Carousel
+        'upload.php'                  // Media
+    );
+}
+add_filter( 'custom_menu_order', '__return_true' );
+add_filter( 'menu_order', 'ngisweden_admin_menu_media_down' );
+
+// Remove certain pages from the menu
+function my_remove_menus() {
+    remove_submenu_page('themes.php', 'themes.php' ); // Theme chooser
+    remove_submenu_page('themes.php', 'theme-editor.php' ); // Theme editor
+    remove_submenu_page('plugins.php', 'plugin-editor.php' ); // Plugin editor
+    remove_submenu_page('options-general.php', 'options-discussion.php' ); // Discussion
+}
+add_action( 'admin_menu', 'my_remove_menus', 999 );
+
+// Remove the annoying boxes from the dashboard index page
+function remove_dashboard_widgets() {
+    // remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );   // At a Glance
+    remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );  // Quick Draft
+    remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' );   // Activity
+    remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );      // WordPress Events and News
+    // These don't seem to exist for me? But were on the WordPress example, so figure it doesn't hurt to keep
+    remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' ); // Recent Comments
+    remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );  // Incoming Links
+    remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );         // Plugins
+    remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );     // Recent Drafts
+    remove_meta_box( 'dashboard_secondary', 'dashboard', 'side' );         // Other WordPress News
+}
+add_action( 'wp_dashboard_setup', 'remove_dashboard_widgets' );
+
+// Don't let people have any choice! muwahahahaha
+remove_action( 'admin_color_scheme_picker', 'admin_color_scheme_picker' );
---
+++ wp-content/themes/ngisweden/functions/shortcodes/publications.php
@@ -0,0 +1,183 @@
+<?php
+// NGI Publications Shortcode
+function ngisweden_pubs_shortcode($atts_raw){
+
+    // Facility labels
+    $fac_labels = array(
+        'NGI Stockholm (Genomics Applications)',
+        'NGI Stockholm (Genomics Production)',
+        'NGI Uppsala (SNP&SEQ Technology Platform)',
+        'NGI Uppsala (Uppsala Genome Center)'
+    );
+    $download_limit = 50;
+
+    // Shortcode attribute defaults
+    $atts = shortcode_atts( array(
+        'title' => 1,
+        'randomise' => 1,
+        'num' => 5,
+        'collabs' => 0
+    ), $atts_raw);
+
+    // Fetch the cached publications data
+    $pubs_json = @file_get_contents(get_template_directory().'/publications_cache.json');
+    $pubs_data = @json_decode($pubs_json, true);
+
+    // Refresh cache if it doesn't exist or is more than a week old
+    if(!$pubs_data or $pubs_data['downloaded'] < (time()-(60*60*24*7))){
+        $pubs_data = array(
+            'downloaded' => time(),
+            'publications' => array()
+        );
+        foreach($fac_labels as $fac){
+            $pubs_url = 'https://publications.scilifelab.se/label/'.rawurlencode($fac).'.json?limit='.$download_limit;
+            $pubs_json = file_get_contents($pubs_url);
+            $pubs_raw_data = json_decode($pubs_json, true);
+            $pubs_data['publications'] = array_merge($pubs_data['publications'], $pubs_raw_data['publications']);
+        }
+
+        // Clean up
+        $pub_ids = array();
+        foreach($pubs_data['publications'] as $idx => $pub){
+            // Remove duplicates
+            if(in_array($pub['iuid'], $pub_ids)){
+                unset($pubs_data['publications'][$idx]);
+                continue;
+            }
+            array_push($pub_ids, $pub['iuid']);
+
+            // Check if this is a collaboration
+            $pubs_data['publications'][$idx]['is_collab'] = false;
+            foreach($fac_labels as $fac){
+                if(isset($pub['labels'][$fac]) && $pub['labels'][$fac] == 'Collaborative'){
+                    $pubs_data['publications'][$idx]['is_collab'] = true;
+                }
+            }
+        }
+        @file_put_contents(get_template_directory().'/publications_cache.json', json_encode($pubs_data));
+    }
+
+    // Randomise the order
+    if($atts['randomise']) {
+        shuffle($pubs_data['publications']);
+    }
+
+    // Build output
+    $modals = '';
+    $pubs_items = array();
+    $i = 0;
+    $num_collabs = 0;
+    foreach($pubs_data['publications'] as $pub){
+        // Filter by label if we need to
+        if($atts['collabs'] > 0){
+            $remaining_non_collab = $atts['num'] - $atts['collabs'] - $i;
+            if(!$pub['is_collab'] && $remaining_non_collab <= 0){
+                continue;
+            }
+        }
+
+        // Limit the number shown
+        if($i >= $atts['num']){
+            break;
+        }
+
+        // Bump the counter
+        $i++;
+
+        // Add to the visible list
+        $pubs_items[] = '
+        <a data-toggle="modal" data-target="#pub_'.$pub['iuid'].'" href="'.$pub['links']['display']['href'].'" target="_blank" class="list-group-item list-group-item-action'.($pub['is_collab'] ? ' list-pub-collab' : '').'">
+            '.$pub['title'].'<br>
+            <small class="text-muted"><em>'.$pub['journal']['title'].'</em> ('.explode('-', $pub['published'])[0].')</small>
+        </a>';
+
+        //
+        // Make publication modal
+        //
+
+        // Make authors array
+        $authors = array();
+        foreach($pub['authors'] as $author){
+            // If ALL CAPS then capitilise nicely
+            if(strtoupper($author['given']) == $author['given']){
+                $author['given'] = ucwords(strtolower($author['given']));
+            }
+            if(strtoupper($author['family']) == $author['family']){
+                $author['family'] = ucwords(strtolower($author['family']));
+            }
+            $authors[] = '<span class="pub-author" data-toggle="tooltip" title="'.$author['given'].' '.$author['family'].'">'.$author['initials'].' '.$author['family'].'</span>';
+        }
+
+        // Make publication ref string
+        $pub_ref = '';
+        if($pub['journal']['title']){
+            $pub_ref .= '<em>'.$pub['journal']['title'].'</em>, ';
+        }
+        $pub_ref .= '<small>';
+        if($pub['journal']['volume']){
+            $pub_ref .= '<strong>'.$pub['journal']['volume'].'</strong> ';
+        }
+        if($pub['journal']['issue']){
+            $pub_ref .= '('.$pub['journal']['issue'].') ';
+        }
+        if($pub['journal']['issn']){
+            $pub_ref .= $pub['journal']['issn'].' ';
+        }
+        if($pub['published']){
+            $pub_ref .= '('.explode('-', $pub['published'])[0].')';
+        }
+        $pub_ref .= '</small>';
+
+        // NGI collaboration flag
+        $collab_badge = '';
+        if($pub['is_collab']){
+            $collab_badge = '<span class="float-right badge badge-primary">NGI Collaboration</span>';
+        }
+
+        // Only show modal body if we have an abstract
+        $abstract = '';
+        if($pub['abstract']){
+            $abstract = '<div class="modal-body small">'.$pub['abstract'].'</div>';
+        }
+        $modals .= '
+        <div class="modal fade" id="pub_'.$pub['iuid'].'" tabindex="-1" role="dialog" aria-hidden="true">
+          <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <div class="modal-title">
+                    <h5>'.$pub['title'].'</h5>
+                    <p class="font-weight-light pub-authors">'.implode(', ', $authors).'</p>
+                    <p class="mb-0">'.$collab_badge.$pub_ref.'</p>
+                </div>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              </div>
+              '.$abstract.'
+              <div class="modal-footer">
+                <button type="button" class="btn btn-sm btn-secondary" data-dismiss="modal">Close</button>
+                <a href="https://www.ncbi.nlm.nih.gov/pubmed/'.$pub['pmid'].'" target="_blank" class="btn btn-sm btn-info">Pubmed <i class="fas fa-external-link-alt fa-sm ml-2"></i></a>
+                <a href="https://dx.doi.org/'.$pub['doi'].'" target="_blank" class="btn btn-sm btn-primary">DOI <i class="fas fa-external-link-alt fa-sm ml-2"></i></a>
+                <a href="'.$pub['links']['display']['href'].'" target="_blank" class="btn btn-sm btn-success">SciLifeLab Pubs <i class="fas fa-external-link-alt fa-sm ml-2"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>';
+    }
+
+    // Randomise the order again, so that collabs aren't always at the bottom
+    if($atts['randomise']) {
+        shuffle($pubs_items);
+    }
+
+    // Build the pubs list
+    $pubs_div = '<div class="ngisweden-publications mb-5">';
+    if($atts['title']) {
+        $pubs_div .= '<h5>User Publications</h5>';
+    }
+    $pubs_div .= '<div class="list-group">';
+    $pubs_div .= implode("\n", $pubs_items);
+    $pubs_div .= '</div></div>';
+
+    // Return the list and modals output
+    return $pubs_div.$modals;
+}
+add_shortcode('ngisweden_publications', 'ngisweden_pubs_shortcode');
