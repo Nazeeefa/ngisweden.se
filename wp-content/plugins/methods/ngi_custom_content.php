5b782e4e3573725e2354bc6a958571fdb0b14dd0
Phil Ewels <phil.ewels@scilifelab.se>
2019-04-06 23:17:07 +0200

    Even more plugin reordering

---
+++ wp-content/plugins/methods/ngi_custom_content.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: NGI Custom Content
+Plugin URI: https://github.com/NationalGenomicsInfrastructure/ngisweden.se
+Description: Plugin to handle administration, submission and display of library prep methods on the NGI website.
+Version: 1.0
+Author: Phil Ewels
+Author URI: http://phil.ewels.co.uk
+License: MIT
+*/
+
+// Methods post type
+require_once('methods.php');
+
+// Bioinformatics post type
+require_once('bioinformatics.php');
+
+// Add in the taxonomies
+require_once('taxonomies/applications.php');
+require_once('taxonomies/seqtype.php');
+require_once('taxonomies/status.php');
+require_once('taxonomies/keywords.php');
+
+// Link methods and bioinformatics
+require_once('methods_bioinfo_linking.php');
