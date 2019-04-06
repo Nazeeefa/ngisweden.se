745fcb9e250d2bdb46babd87c3f59a7d36a4ed22
Phil Ewels <phil.ewels@scilifelab.se>
2019-04-06 15:27:03 +0200

    Make the news archive page a little more functional

--- wp-content/themes/ngisweden/home.php
+++ wp-content/themes/ngisweden/home.php
@@ -6,22 +6,39 @@
 
 get_header(); ?>
 
-<div class="container main-page">
-
-  <h1>News</h1>
-
-  <?php
-  if (have_posts()) {
-    while (have_posts()) {
-      the_post();
-      echo '<h4>'.get_the_title().'</h4>';
-      if(get_the_excerpt() and strlen(trim(get_the_excerpt()))){
-        the_excerpt();
-      }
-    }
-  }
-  ?>
-
+<div class="ngisweden-sidebar-page">
+  <div class="container main-page">
+    <div class="row">
+      <div class="col-sm-9">
+        <h1>News</h1>
+        <hr>
+        <?php
+        if (have_posts()) {
+          while (have_posts()) {
+            the_post();
+            echo '<h4><a href="'.get_the_permalink().'">'.get_the_title().'</a></h4>';
+            if ( has_post_thumbnail() ) {
+              echo '<a href="'.get_the_permalink().'" class="alignright">'.get_the_post_thumbnail('thumb').'</a>';
+            }
+            the_excerpt();
+            echo '<p class="small text-muted">'.get_the_date().' &nbsp;-&nbsp; Categories: <em>'.get_the_category_list(', ').'</em></p>';
+            echo '<hr>';
+          }
+        }
+        ?>
+      </div>
+      <div class="col-sm-3 ngisweden-sidebar-page-sidebar">
+        <h5 class="mt-3">News Archives</h5>
+        <ul>
+          <?php
+          wp_get_archives(array(
+            'type' => 'yearly'
+          ));
+          ?>
+        </ul>
+      </div>
+    </div>
+  </div>
 </div>
 
 <?php get_footer();
